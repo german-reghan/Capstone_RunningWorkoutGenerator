d25d261f62da714ca019106339a832ec71fd5ae6
german-reghan <germanreghan8@gmail.com>
2026-07-19 17:17:36 -0400

    Added in an array of 5k workouts

---
+++ js/workouts.js
@@ -0,0 +1,60 @@
+const workouts = [
+    {
+        distance: "5k",
+        level: "beginner",
+        terrain: "hilly",
+        title: "Hill Sprints for 5k",
+        workout: "Start with a 1 mile warm-up. Start at the bottom of a hill that will take at least 30 seconds to run up. Sprint up the hill for 30 seconds. Jog down for recovery. Run 6 reps. Cool down for 1-2 miles.",
+        source: "marathonhandbook.com",
+    },
+
+    {
+        distance: "5k",
+        level: "beginner",
+        terrain: "track",
+        title: "1000 Meter Repeats",
+        workout: "Start with a 1 mile warm-up. Run 3 x 1000 meters at your target race pace. After each rep, take 90 seconds recovery (walk). Cool down run for 1 mile.",
+        source: "marathonhandbook.com",     
+    },
+
+    {
+        distance: "5k",
+        level: "beginner",
+        terrain: "flat road",
+        title: "1 Minute Repeats",
+        workout: "Start with a 1 mile warm-up. Now you will run hard for 10 intervals of 1 minute each. Run 1 minute hard. Then walk for one minute. Repeat x10. Cool down run for 1 mile.",
+        source: "therunningchannel.com",
+    },
+
+    {
+        distance: "5k",
+        level: "intermediate",
+        terrain: "hilly",
+        title: "Hill Sprints for 5K",
+        workout: "Start with a 2 mile warm-up. Start at the bottom of a hill that will take 45 seconds to run up. Sprint up the hill for 45 seconds. Jog down for recovery. Run 10-12 reps. Cool down for 2 miles.",
+        source: "marathonhandbook.com",
+    },
+
+    {
+        distance: "5k",
+        level: "intermediate",
+        terrain: "track",
+        title: "1000 Meter Repeats",
+        workout: "Start with a 2 mile warm-up. Run 5 x 1000 meters at your target race pace. After each rep, take 60-90 seconds recovery (walk). Cool down run for 2 miles.",
+        source: "marathonhandbook.com",
+    },
+
+    {
+        distance: "5k",
+        level: "intermediate",
+        terrain: "flat road",
+        title: "2 Minute Repeats",
+        workout: "Start with a 2 mile warm-up. Now you will run hard for 10 intervals of 2 minutes each. Run 2 minutes hard. Then walk for one minute. Repeat x10. Cool down run for 2 miles.",
+        source: "the runningchannel.com",
+    },
+
+
+
+
+
+]
